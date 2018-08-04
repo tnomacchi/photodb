16a8657c990189a55e8cf3a152f745d010533dc6
Takashi Noma <takashi.noma@gmail.com>
2018-08-04 15:43:42 +0900

    chage input button

--- post.php
+++ post.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ファイルアップロード</title>
+<style>
+label > input {
+	display:none;
+}
+label {
+	color: white;
+	background-color: #4444AA;
+	padding: 20px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Hiragino Sans', 'Hiragino Kaku Gothic ProN', '游ゴシック  Medium', meiryo, sans-serif;
+}
+</style>
+</head>
+<body>
+<h1>TEST</h1>
 <?php
 try{
     if(is_uploaded_file($_FILES['file']['tmp_name'])){
@@ -12,6 +31,11 @@ if($_FILES['file']){
 }
 ?>
 <form action="./post.php" method="POST" enctype="multipart/form-data">
-  <input type="file" name="file" accept="image/*"  capture="environment" />
+  
+  <label for="file_photo">カメラで撮影
+  <input type="file" name="file" accept="image/*" id="file_photo" capture="environment" />
+  </label>
   <input type="submit" value="ファイルをアップロードする">
 </form>
+</body>
+</html>
